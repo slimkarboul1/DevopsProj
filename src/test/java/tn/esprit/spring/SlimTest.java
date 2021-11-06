299d3ef121ffc9ddd751dca269ed073cd3e9cda5
Slim Karboul <74094245+slimkarboul1@users.noreply.github.com>
2021-11-06 17:23:07 +0100

    Update SlimTest.java

--- src/test/java/tn/esprit/spring/SlimTest.java
+++ src/test/java/tn/esprit/spring/SlimTest.java
@@ -3,6 +3,7 @@ package tn.esprit.spring;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertNull;
 
+import java.util.Date;
 import java.util.List;
 import java.util.NoSuchElementException;
 
@@ -14,61 +15,79 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import tn.esprit.spring.entities.Timesheet;
+import tn.esprit.spring.entities.TimesheetPK;
+import tn.esprit.spring.repository.EmployeRepository;
+import tn.esprit.spring.repository.MissionRepository;
+import tn.esprit.spring.repository.TimesheetRepository;
 import tn.esprit.spring.entities.Departement;
 import tn.esprit.spring.entities.Employe;
 import tn.esprit.spring.entities.Entreprise;
+import tn.esprit.spring.entities.Mission;
+import tn.esprit.spring.entities.MissionExterne;
 import tn.esprit.spring.entities.Role;
 import tn.esprit.spring.services.IDepartementService;
 import tn.esprit.spring.services.IEmployeService;
 import tn.esprit.spring.services.IEntrepriseService;
+import tn.esprit.spring.services.ITimesheetService;
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class KhalilTest {
-	private static final Logger l = LogManager.getLogger(KhalilTest.class);
+public class SlimTest {
 	@Autowired
-	IEmployeService iemployeservice;
-	
-	@Autowired
-	IEntrepriseService ientrepriseservice;
+	ITimesheetService itimesheetservice;
 	@Autowired
 	IDepartementService idepartmentService;
-	@Test
-	public void testContextLoads() {
-		Entreprise ent = new Entreprise("esprit", "test");
-		int idEnt =ientrepriseservice.ajouterEntreprise(ent) ;
-	    assertThat(idEnt).isNotEqualTo(-1);
-	    
-	    
-		
-		Departement dep = new Departement("informatique");
-		int idDep = this.idepartmentService.addOrUpdateDepartment(dep);
-		
-		ientrepriseservice.affecterDepartementAEntreprise(idDep, idEnt);
-		l.info("started adding employee test");
-		Employe empl = new Employe("khalil", "sayhi", "khalil.sayhi1@esprit.tn", "khalil", true,Role.CHEF_DEPARTEMENT );
-		int id = this.iemployeservice.addOrUpdateEmploye(empl);
-		 this.iemployeservice.affecterEmployeADepartement(id, idDep);
-		 assertThat(empl.getNom()).isEqualTo("khalil");
-		 l.info("affection reussite done");
-	}
-	@Test
-	public void editEmploye() {
-		l.info("editing employe test");
-		
-		List<Employe> myList = this.iemployeservice.getAllEmployes();
-		String mail = "new email";
-		Employe empToEdit = myList.get(0);
-		this.iemployeservice.mettreAjourEmailByEmployeId(mail, empToEdit.getId());
-		Employe empToTest = this.iemployeservice.getEmployeById(empToEdit.getId()).get();
-		assertThat(empToTest.getEmail()).isEqualTo("new email");
-	}
+	@Autowired
+	IEmployeService iemployeservice;
+	@Autowired
+	MissionRepository missionrepo;
 	
-	@Test(expected = NoSuchElementException.class)
-	public void deleteEmploye() {
-		l.info("deleting employe test");
-		Employe empToEdit = this.iemployeservice.getAllEmployes().get(0);
-		this.iemployeservice.deleteEmployeById(empToEdit.getId());
-		Employe empToTest = this.iemployeservice.getEmployeById(empToEdit.getId()).get();
-		assertNull(empToTest);
+	private static final Logger log = LogManager.getLogger(SlimTest.class);
+	
+//	@Test
+//	public void testContextLoads() {
+//		
+//		Date datedebut = new Date(2021,2,11);
+//		Date datefin = new Date(2021,6,11);
+//		TimesheetPK pk = new TimesheetPK(1,1,datedebut,datefin);
+//		Departement dep = new Departement("informatique");
+//		int idDep = this.idepartmentService.addOrUpdateDepartment(dep);
+//		
+//		Employe empl = new Employe("slim", "karboul", "slim.karboul@esprit.tn", "slim", true,Role.CHEF_DEPARTEMENT );
+//		int id = this.iemployeservice.addOrUpdateEmploye(empl);
+//		 this.iemployeservice.affecterEmployeADepartement(id, idDep);
+//		
+//		 
+//		 	MissionExterne mexterne = new MissionExterne("m1","m1","email",1);
+//		 	missionrepo.save(mexterne);
+//			Timesheet tms = new Timesheet(pk,mexterne, empl, true);
+//			itimesheetservice.ajouterTimesheet(mexterne.getId(), empl.getId(), datedebut, datefin);
+//		
+//        
+//	}
+//	
+//	@Test
+//	public void editTimesheet() {
+//		log.info("editing timesheet test");
+//		
+//		List<Employe> myList = this.itimesheetservice.getAllEmployeByMission(1);
+//		String mail = "new new email";
+//		Employe employeEdit = myList.get(0);
+//		this.iemployeservice.mettreAjourEmailByEmployeId(mail, employeEdit.getId());
+//		Employe empToTest = this.iemployeservice.getEmployeById(employeEdit.getId()).get();
+//		assertThat(empToTest.getEmail()).isEqualTo("new new email");
+//	}
+	
+	@Test
+	public void delteTimesheet() {
+		Employe emp = this.iemployeservice.getAllEmployes().get(0);
+		Date datedebut = new Date(2021,2,11);
+		Date datefin = new Date(2021,6,11);
+		log.info("deleting timesheet test");
+		TimesheetPK pk = new TimesheetPK(1,emp.getId(),datedebut,datefin);
+		Timesheet timesheet = this.itimesheetservice.getAllTimeSheet().get(0);
+		this.itimesheetservice.deleteTimesheetById(timesheet);
+
 	}
 }
+
